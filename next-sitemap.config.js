496ce3b12e4f412f8f2bc80b0f7fc78d3fb4e107
yg0a1n <web3.flexbiz@gmail.com@gmail.com>
2024-05-02 17:21:16 +0200

    fix: launch postbuild

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,11 @@
-const siteUrl = process.env.SITE_URL || 'http://localhost:3000'
+const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl,
   generateRobotsTxt: true,
   robotsTxtOptions: {
-    policies: [
-      { userAgent: '*', allow: '/' },
-    ],
-    additionalSitemaps: [
-      `${siteUrl}/sitemap.xml`,
-    ],
+    policies: [{ userAgent: '*', allow: '/' }],
+    additionalSitemaps: [`${siteUrl}/sitemap.xml`],
   },
 }
